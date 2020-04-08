40b37c85a07934b5f0087be36e54e8d540d41a40
Eli Entelis <elientelis1999@gmail.com>
2020-04-09 02:17:57 +0300

    added bcrypt package

--- index.js
+++ index.js
@@ -1,45 +1,43 @@
-var express = require('express'),
+const express = require("express"),
   app = express(),
   port = process.env.PORT || 5000,
-  mongoose = require('mongoose'),
-  User = require('./models/user.model.js'), //created model loading here
-  bodyParser = require('body-parser');
+  mongoose = require("mongoose"),
+  User = require("./models/user.model.js"), //created model loading here
+  bodyParser = require("body-parser");
 
 
-  
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/shampoo'); 
-
+mongoose.connect("mongodb://localhost/shampoo");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Add headers
-app.use(function (req, res, next) {
-
+app.use(function(req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader("Access-Control-Allow-Headers", "*");
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
   // Pass to next layer of middleware
   next();
 });
 
-var routes = require('./routes/user.route.js'); //importing route
+var routes = require("./routes/user.route.js"); //importing route
 routes(app); //register the route
-
 
 app.listen(port);
 
-
-console.log('user list RESTful API server started on: ' + port);
+console.log("user list RESTful API server started on: " + port);
