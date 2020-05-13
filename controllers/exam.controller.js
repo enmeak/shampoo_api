19bd39d3af49e3624305220c4a97952f4e76e266
Eli Entelis <elientelis1999@gmail.com>
2020-05-13 23:07:46 +0300

    added endpoint for specific exam

--- controllers/exam.controller.js
+++ controllers/exam.controller.js
@@ -2,27 +2,34 @@ var mongoose = require("mongoose"),
   Exam = mongoose.model("Exams");
 
 // function that return exam by id
-
+exports.get_exam_by_id = function (req, res) {
+  Exam.findOne({ _id: req.params.exam_id }, function (err, exam) {
+    if (err) res.send(err);
+    else {
+      res.send(exam);
+    }
+  });
+};
 // function to return all exam names under each team
 
 // function to save exam in db
-exports.add_exam = function(req, res) {
-    let new_exam = new Exam(req.body);
-    console.log(new_exam);
-    
-    new_exam.save(function(err, task) {
-      if (err) res.send(err);
-      else res.send("exam created");
-    });
-  };
-  
-  exports.get_all_exams = function(req, res) {
-    Exam.find({}, function(err, users) {
-      if (err) res.send(err);
-      else {
-        res.send(users);
-      }
-    });
-  };
+exports.add_exam = function (req, res) {
+  let new_exam = new Exam(req.body);
+  console.log(new_exam);
 
-  // function to delete exam
+  new_exam.save(function (err, task) {
+    if (err) res.send(err);
+    else res.send("exam created");
+  });
+};
+
+exports.get_all_exams = function (req, res) {
+  Exam.find({}, function (err, exams) {
+    if (err) res.send(err);
+    else {
+      res.send(exams);
+    }
+  });
+};
+
+// function to delete exam
--- routes/exam.route.js
+++ routes/exam.route.js
@@ -1,13 +1,12 @@
-module.exports = function(app) {
-    var examFunctions = require("../controllers/exam.controller.js");
-  
-    // save exam route
-    app
-      .route("/exams/add_exam")
-      .post(examFunctions.add_exam);
-    app
-      .route("/exams/")
-      .get(examFunctions.get_all_exams);
-  
-  };
-  
+module.exports = function (app) {
+  var examFunctions = require("../controllers/exam.controller.js");
+
+  // save exam route
+  app.route("/exams/add_exam").post(examFunctions.add_exam);
+
+  // get the full colletion of exams
+  app.route("/exams/").get(examFunctions.get_all_exams);
+
+  // get a specific exam by id
+  app.route("/exams/:exam_id").get(examFunctions.get_exam_by_id);
+};
